4ce3a36300d3877a32fc5a0af7986cc90d2ab1c7
andsou <andysoutcharith@gmail.com>
2022-04-09 23:30:40 -0500

    Javascript for the sketch

---
+++ script.js
@@ -0,0 +1,7 @@
+let drawingGrid = document.getElementById("drawing-grids");
+
+for (i = 0; i <= 16; i++) {
+    let square = document.createElement("div");
+
+    drawingGrid.appendChild(square);
+}
