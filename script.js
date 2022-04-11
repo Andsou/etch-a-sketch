0fecff4435b4789f9853248fbad4fb1e6bb42fcc
andsou <andysoutcharith@gmail.com>
2022-04-11 01:28:13 -0500

    Added clear button at the top of the page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let drawingGrid = document.getElementById("drawing-grids");
 
 
 // Create 256 square divs
-for (i = 0; i < 256; i++) {
+for (let i = 0; i < 225; i++) {
     let square = document.createElement("div");
     square.className = "square";
 
@@ -17,15 +17,26 @@ let mouseDown = false;
 allSqaures.forEach((square) => {
     square.addEventListener("mouseover", (e) => {
         if(e.buttons == 1 || e.buttons == 3){
-            square.style.backgroundColor = '#616466';
+            square.style.backgroundColor = '#1b1b1b';
         }
     });
 });
 
 allSqaures.forEach((square) => {
     square.addEventListener("mousedown", () => {
- 
-        square.style.backgroundColor = '#616466';
-
+        square.style.backgroundColor = '#1b1b1b';
     });
 });
+
+let promptMe = () => {
+    let user = prompt("Enter number between 1 and 100 to set the size");
+    const testInput = new RegExp(/^\d+$/);
+
+    if (!testInput.test(user) || user == null) {
+        user = prompt("Enter number between 1 and 100 to set the size");
+    }
+    
+    allSqaures.forEach((square) => {
+        square.style.backgroundColor = "rgb(240, 238, 238)";
+    });
+}
