7d1c74348c7a1e55e35268f4511fcec3bd88ebe9
andsou <andysoutcharith@gmail.com>
2022-04-11 18:38:36 -0500

    Clear function now prompts the user for a grid size between 1 and 100

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
 let drawingGrid = document.getElementById("drawing-grids");
 
-
-// Create 256 square divs
-for (let i = 0; i < 225; i++) {
-    let square = document.createElement("div");
-    square.className = "square";
-
-    drawingGrid.appendChild(square);
+let setDrawingBoard = (size) => {
+    for (let i = 0; i < size; i++) {
+        let square = document.createElement("div");
+        square.className = "square";
+    
+        drawingGrid.appendChild(square);
+    }
 }
 
-let allSqaures = document.querySelectorAll(".square");
-let hoverEffect = '.square :hover{ background-color: #00ff00 }';
-let mouseDown = false;
+let setInk = () => {
+    let allSqaures = document.querySelectorAll(".square");
 
+    allSqaures.forEach((square) => {
+        square.addEventListener("mouseover", (e) => {
+            if(e.buttons == 1  || square.style.backgroundColor == '#1b1b1b'){
+                square.style.backgroundColor = '#1b1b1b';
+            }
+        });
+    });
 
-allSqaures.forEach((square) => {
-    square.addEventListener("mouseover", (e) => {
-        if(e.buttons == 1 || e.buttons == 3){
+    allSqaures.forEach((square) => {
+        square.addEventListener("mousedown", () => {
             square.style.backgroundColor = '#1b1b1b';
-        }
+            console.log('success');
+        });
     });
-});
+}
 
-allSqaures.forEach((square) => {
-    square.addEventListener("mousedown", () => {
-        square.style.backgroundColor = '#1b1b1b';
-    });
-});
 
-let promptMe = () => {
-    let user = prompt("Enter number between 1 and 100 to set the size");
-    const testInput = new RegExp(/^\d+$/);
+setInk();
 
-    if (!testInput.test(user) || user == null) {
-        user = prompt("Enter number between 1 and 100 to set the size");
-    }
+document.getElementById("clear-button").addEventListener("click", function() {
+    let allSqaures = document.querySelectorAll(".square");
+
+    do {
+        var userNumber = parseInt(window.prompt("Enter number between 1 and 100 to set the size", ""), 10);
+    } while(isNaN(userNumber) || userNumber > 100 || userNumber < 1);
+
+    drawingGrid.style.gridTemplateColumns = "repeat(" + userNumber + ", auto)";
+    drawingGrid.style.gridTemplateRows = "repeat(" + userNumber + ", fill)";
+    setDrawingBoard(userNumber * userNumber);
+    setInk();
     
     allSqaures.forEach((square) => {
         square.style.backgroundColor = "rgb(240, 238, 238)";
     });
-}
+});
+
+
+
+
